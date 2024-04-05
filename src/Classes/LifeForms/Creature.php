cc13319ead55c087818d6136c2636aa34b50b009
Stepan <pirogov1923@mail.ru>
2024-04-05 19:05:43 +0500

    изменения базового класса Creature

--- src/Classes/LifeForms/Creature.php
+++ src/Classes/LifeForms/Creature.php
@@ -1,15 +1,24 @@
 <?php
 
 namespace App\Classes\LifeForms;
+use App\Classes\Core\Coordinate;
+use App\Classes\Core\Entity;
+use App\Classes\Core\Map\MapInterface;
+use App\Classes\Core\PathAlgorithms\PathAlgorithmInterface;
+
 abstract class Creature extends Entity
 {
     protected int $speed;
     protected int $hp;
-
-    abstract public function makeMove();
-
-    public function damage(int $power): void
+    protected bool $hunger;
+    protected string $target;
+    protected Coordinate $coordinate;
+    protected MapInterface $map;
+    protected PathAlgorithmInterface $algorithm;
+    protected int $remainingSteps;
+    abstract public function makeActions(bool $isEat, int $remainingSteps);
+    public function getSpeed(): int
     {
-        $this->hp-=$power;
+        return $this->speed;
     }
 }
