519e23afa2700f49264423f1297c355b966c4852
Stepan <pirogov1923@mail.ru>
2024-04-05 19:06:43 +0500

    вспомогательный класс для определения положения объектов

---
+++ src/Classes/Core/Coordinate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Classes\Core;
+
+class Coordinate
+{
+    private int $x;
+    private int $y;
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+}
