4571a349838e13e3434b32ad2444fcafce4df350
Stepan <pirogov1923@mail.ru>
2024-04-06 13:41:12 +0500

    добавление свойств в Coordinate

--- src/Classes/Core/Coordinate.php
+++ src/Classes/Core/Coordinate.php
@@ -6,10 +6,15 @@ class Coordinate
 {
     private int $x;
     private int $y;
+
+    private ?Coordinate $parent;
+
+
     public function __construct(int $x, int $y)
     {
         $this->x = $x;
         $this->y = $y;
+        $this->parent = null;
     }
 
     public function getX(): int
@@ -22,4 +27,13 @@ class Coordinate
         return $this->y;
     }
 
+    public function getParent(): ?Coordinate
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?Coordinate $parent): void
+    {
+        $this->parent = $parent;
+    }
 }
