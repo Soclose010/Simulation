e71b94590d189c996c0c7b24ea620552ffe0aa39
Stepan <pirogov1923@mail.ru>
2024-04-08 16:32:35 +0500

    расширен интерфейс MapInterface, реализован класс Map

--- src/Classes/Core/Map/Map.php
+++ src/Classes/Core/Map/Map.php
@@ -2,7 +2,41 @@
 
 namespace App\Classes\Core\Map;
 
-class Map
-{
+use App\Classes\LifeForms\Entity;
+use App\Classes\LifeForms\Food\Food;
 
+class Map implements MapInterface
+{
+    /**
+     * @var Entity[]
+     */
+    private array $entities = [];
+
+    public function remove(Entity $entity): void
+    {
+        $this->entities = array_filter($this->entities, function ($ent) use ($entity) {
+            return $ent->getCords()->getStringCords() === $entity->getCords()->getStringCords();
+        });
+    }
+
+    public function add(Entity $entity): void
+    {
+        $this->entities[] = $entity;
+    }
+
+    public function newTurn(): void
+    {
+        $this->clear();
+    }
+
+    private function clear(): void
+    {
+        $this->entities = array_filter($this->entities, function ($entity) {
+            if ($entity instanceof Food)
+            {
+                return $entity->getWeight() > 0;
+            }
+            return true;
+        });
+    }
 }
--- src/Classes/Core/Map/MapInterface.php
+++ src/Classes/Core/Map/MapInterface.php
@@ -3,8 +3,14 @@
 namespace App\Classes\Core\Map;
 
 use App\Classes\Core\Coordinate;
+use App\Classes\LifeForms\Entity;
+use App\Classes\LifeForms\Food\Food;
 
 interface MapInterface
 {
-    public function remove(Coordinate $coordinate) : void;
+    public function remove(Entity $entity) : void;
+    public function add(Entity $entity): void;
+
+    public function newTurn(): void;
+
 }
