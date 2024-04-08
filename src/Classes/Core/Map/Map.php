c13aeb525243c797f5a7d700ead7ae18bbe0b56a
Stepan <pirogov1923@mail.ru>
2024-04-08 19:17:44 +0500

    расширен интерфейс MapInterface, реализованы методы в Map

--- src/Classes/Core/Map/Map.php
+++ src/Classes/Core/Map/Map.php
@@ -2,6 +2,7 @@
 
 namespace App\Classes\Core\Map;
 
+use App\Classes\Core\Coordinate;
 use App\Classes\LifeForms\Entity;
 use App\Classes\LifeForms\Food\Food;
 
@@ -12,10 +13,20 @@ class Map implements MapInterface
      */
     private array $entities = [];
 
+    private Coordinate $start;
+
+    private Coordinate $end;
+
+    public function __construct(Coordinate $start, Coordinate $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
     public function remove(Entity $entity): void
     {
         $this->entities = array_filter($this->entities, function ($ent) use ($entity) {
-            return $ent->getCords()->getStringCords() === $entity->getCords()->getStringCords();
+            return $ent->getCoordinate()->getStringCords() === $entity->getCoordinate()->getStringCords();
         });
     }
 
@@ -38,5 +49,54 @@ class Map implements MapInterface
             }
             return true;
         });
+    }
+
+    public function move(Entity $entity, Coordinate $coordinate): void
+    {
+        $entity->setCoordinate($coordinate);
+    }
+
+    public function getEntityByCords(Coordinate $coordinate): Entity
+    {
+        return array_filter($this->entities, function ($entity) use ($coordinate)
+        {
+            return $entity->getCoordinate()->getStringCords() === $coordinate->getStringCords();
+        })[0];
+    }
+
+    public function getEntityBoundaries(Entity $entity): array
+    {
+        $startCords = $this->start->getArrayCords();
+        $endCords = $this->end->getArrayCords();
+        $entityCords = $entity->getCoordinate()->getArrayCords();
+        $cordsCount = count($startCords);
+        $resCords = [];
+        for ($i = 0; $i < $cordsCount; $i++)
+        {
+            $resCords = array_merge($resCords, $this->getCordWithBoundaries($startCords[$i], $endCords[$i], $entityCords[$i]));
+        }
+        return $resCords;
+    }
+
+    private function getCordWithBoundaries(int $startCord, int $endCord, int $entityCord) : array
+    {
+        $res = [];
+        if ($entityCord > $startCord)
+        {
+            $res[] = $entityCord - 1;
+        }
+        else
+        {
+            $res = $startCord;
+        }
+        if ($entityCord < $endCord)
+        {
+            $res[] = $entityCord + 1;
+        }
+        else
+        {
+            $res = $entityCord;
+        }
+        return $res;
     }
 }
--- src/Classes/Core/Map/MapInterface.php
+++ src/Classes/Core/Map/MapInterface.php
@@ -4,13 +4,17 @@ namespace App\Classes\Core\Map;
 
 use App\Classes\Core\Coordinate;
 use App\Classes\LifeForms\Entity;
-use App\Classes\LifeForms\Food\Food;
-
 interface MapInterface
 {
     public function remove(Entity $entity) : void;
     public function add(Entity $entity): void;
 
     public function newTurn(): void;
+
+    public function move(Entity $entity, Coordinate $coordinate);
+
+    public function getEntityByCords(Coordinate $coordinate) : Entity;
+
+    public function getEntityBoundaries(Entity $entity) : array;
 
 }
