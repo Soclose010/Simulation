21878ac6dbb36d682d668eb60e67e853049f6551
Stepan <pirogov1923@mail.ru>
2024-04-11 15:38:29 +0500

    расширен класс Entity для отображения иконок

--- src/Classes/LifeForms/Entity.php
+++ src/Classes/LifeForms/Entity.php
@@ -5,22 +5,34 @@ use App\Classes\Core\Coordinate;
 
 abstract class Entity
 {
-
     protected Coordinate $coordinate;
+    protected string $visual;
+
+    protected string $name;
 
     public function __construct(Coordinate $coordinate)
     {
         $this->coordinate = $coordinate;
     }
-
-
     public function getCoordinate(): Coordinate
     {
         return $this->coordinate;
     }
-
     public function setCoordinate(Coordinate $coordinate): void
     {
         $this->coordinate = $coordinate;
+    }
+    public function visualize(): string
+    {
+        return $this->visual;
+    }
+
+    public function visualizeDead(): void
+    {
+        $this->visual = "🥩";
+    }
+    public function getName(): string
+    {
+        return $this->name;
     }
 }
