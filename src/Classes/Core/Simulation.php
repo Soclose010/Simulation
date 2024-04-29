a47bbb51475763e92b40295b9d7a7540853eda65
Stepan <pirogov1923@mail.ru>
2024-04-29 14:06:02 +0500

    Изменение зависимостей класса Simulation

--- src/Classes/Core/Simulation.php
+++ src/Classes/Core/Simulation.php
@@ -2,10 +2,8 @@
 
 namespace App\Classes\Core;
 
-use App\Classes\Core\Actions\InitAction;
-use App\Classes\Core\Actions\TurnAction;
+use App\Classes\Core\Actions\ActionInterface;
 use App\Classes\Core\Map\MapInterface;
-use App\Classes\Core\Printer\PrinterInterface;
 use App\Classes\Core\Render\RenderInterface;
 
 class Simulation
@@ -14,36 +12,36 @@ class Simulation
     private int $turnCounter;
     private RenderInterface $renderer;
 
-    private PrinterInterface $printer;
+    private ActionInterface $initAction;
+    private ActionInterface $turnAction;
 
-    public function __construct(MapInterface $map, RenderInterface $renderer, PrinterInterface $printer)
+    public function __construct(MapInterface $map, RenderInterface $renderer, ActionInterface $initAction, ActionInterface $turnAction)
     {
         $this->map = $map;
         $this->renderer = $renderer;
         $this->turnCounter = 0;
-        $this->printer = $printer;
+        $this->initAction = $initAction;
+        $this->turnAction = $turnAction;
     }
 
     public function initAction(): void
     {
-        $action = new InitAction($this->map, $this->printer);
-        $action->make();
+        $this->initAction->make();
         echo "Инициализация поля" . PHP_EOL;
         $this->renderer->render();
     }
 
-    public function turnAction(TurnAction $action): void
+    public function turnAction(): void
     {
         $this->turnCounter++;
         echo "Ход - {$this->turnCounter}" . PHP_EOL;
-        $action->make();
+        $this->turnAction->make();
     }
 
     public function endless(): void
     {
-        $action = new turnAction($this->map, $this->renderer, $this->printer);
         while ($this->map->haveAliveCreatures()) {
-            $this->turnAction($action);
+            $this->turnAction();
         }
     }
 }
